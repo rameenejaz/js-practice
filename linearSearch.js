58e9c62200044403788b05ae008ee3e61b2e5e74
Rameen Ejaz <rameenejaz4@gmail.com>
2025-08-27 01:29:42 +0500

    finding a number in an array by linear search

---
+++ linearSearch.js
@@ -0,0 +1,13 @@
+//using linear search to find a specific element in the array
+let array=[1,5,7,10];
+function linearSearch(array, target) {
+    for (let i=0; i<array.length; i++) {
+        if(target==array[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+let result=linearSearch(array,10);
+console.log("Element found at index: " +result);
