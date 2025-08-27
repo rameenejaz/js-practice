d4b88cacf6c6ddda0b4c9489a50c7caef56469c2
Rameen Ejaz <rameenejaz4@gmail.com>
2025-08-27 23:34:47 +0500

    finding a target by binary search

--- binarySearch.js
+++ binarySearch.js
@@ -2,6 +2,16 @@ function binaryFunction(nums, target) {
     let left=0;
     let right=nums.length-1;
     while (right>=left) {
-        
+        let middle= Math.floor((right+left)/2);
+        if (target==nums[middle]) {
+            return middle;
+        }
+        else if (target==nums[right]) {
+            right=middle-1;
+        }
+        else {
+            left=middle +1;
+        }
     }
+    return -1;
 }
