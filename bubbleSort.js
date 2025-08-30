b67579a7318c1fc1d9b5b2ea218c6679b9cebb80
Rameen Ejaz <rameenejaz4@gmail.com>
2025-08-30 23:58:58 +0500

    using bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,24 @@
+let arr=[5,4,2,1];
+function bubbleSort(arr) {
+    let n=arr.length;
+    for (let i=0; i<n-1; i++) {
+        for (let j=0; j<n-1-i; j++) {
+            if (arr[j]>arr[j+1]) {
+                let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            isSwapped=true;
+            }
+        }
+        if(!isSwapped) {
+            
+            break;
+        }
+    }
+    return arr;
+}
+
+
+let result=bubbleSort(arr);
+console.log("Original Array: " + arr);
+console.log("Sorted Array: " + result);
